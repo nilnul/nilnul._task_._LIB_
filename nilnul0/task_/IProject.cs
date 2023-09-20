7b4e667873d8bd69c762217bc520235104ff7d69
wangyoutian <wangyoutian@qq.com>
2023-09-20 10:18:20 +0000

    Update IProject.cs

--- nilnul0/task_/IProject.cs
+++ nilnul0/task_/IProject.cs
@@ -1,4 +1,4 @@
-﻿namespace nilnul.task_
+namespace nilnul.task_
 {
 	/// <summary>
 	/// project is a way to decompose task, and meanwhile keeping the associations betweeen subtasks such as to make the project an equivalence to the original whole task ;
@@ -7,6 +7,8 @@
 	///  1) a well-defined clear-scoped target;
 	///  2) a constraint of limited resources such as time, cost, etc;
 	///  also, complex enough such that we need to decompose it;
+	///
+	///	this makes task composible and decomposible;
 	/// </remarks>
 	public interface IProject:IRequest4decompose
 		//,IDecomposable or single
