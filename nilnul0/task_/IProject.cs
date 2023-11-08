1b6fb40f0a32a736ef1f91211ffec3dd19330ca8
t0r <t0r@nilnul.org>
2023-11-08 17:20:31 +0800

    better the comment for IProject.cs;

--- nilnul0/task_/IProject.cs
+++ nilnul0/task_/IProject.cs
@@ -4,10 +4,11 @@ namespace nilnul.task_
 	/// project is a way to decompose task, and meanwhile keeping the associations betweeen subtasks such as to make the project an equivalence to the original whole task ;
 	/// </summary>
 	/// <remarks>
+    /// project hence is an optimization problem in that:
 	///  1) a well-defined clear-scoped target;
 	///  2) a constraint of limited resources such as time, cost, etc;
 	///  also, complex enough such that we need to decompose it;
-	///
+	///, and it can be solved using OperationResearch knowledge;
 	///	this makes task composible and decomposible;
 	/// </remarks>
 	public interface IProject:IRequest4decompose
