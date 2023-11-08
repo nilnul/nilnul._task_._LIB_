45dcd48dc62f8fca6f347864afa131fc7f89d63e
WANG,Youtian <wangyoutian@nilnul.com>
2023-11-08 23:04:06 +0800

    added _contact/Role_.cs;

---
+++ _contact/Role_.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nilnul.task._contact
+{
+	internal enum Role_
+	{
+		/// <summary>
+		/// </summary>
+		Initiator
+			,
+		/// <summary>
+		/// </summary>
+		Requester
+			,
+		/// <summary>
+		/// </summary>
+		Creater
+			,
+		/// <summary>
+		/// contact, stakeholders
+		/// </summary>
+		Participant
+			,
+		/// <summary>
+		/// </summary>
+		Owner
+			,
+		Stateholder
+
+	}
+}
