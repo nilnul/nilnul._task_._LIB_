a3f87296bb4ba62ab55edd08058487781aa59301
t0r <t0r@nilnul.org>
2024-09-29 09:19:43 +0800

    added mgt.cs; ..

---
+++ nilnul0/task_/prj/IManagement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nilnul.task_.prj
+{
+    /// <summary>
+    /// manage a project:
+    ///     life cycle in phases: plan, organization(stuffing), control;
+    /// ;manage resources: optimization;
+    /// ;rescources includes human;
+    ///     
+    /// </summary>
+    /// alias:
+    ///     mgt
+    interface IManagement
+    {
+    }
+}
